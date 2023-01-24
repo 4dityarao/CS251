345a21ef34b139d40eaf0bc2bdbbfe52b4350cd6
Aditya Rao <skaditya@gmail.com>
2023-01-24 17:11:20 -0600

    Lab3 Stuff

---
+++ Lab3demo/lab3demo.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<vector>
+#include<array>
+#include<fstream>
+using namespace std;
+
+
+// Array Implementation
+// void songFinder(){
+
+
+//     string album_name,song_name,key;
+//     ifstream infs;
+//     cout <<"Enter the album name (Ends with .txt): "<<endl;
+//     cin>>album_name;
+//     infs.open(album_name);
+//     array<string, 20> track_list;
+//     int i = 0;
+//     while(getline(infs, song_name)){
+//         if(!infs.fail()){
+//             track_list[i] = song_name;
+//             i++;
+//         }
+//     }
+//     cout<<"Album "<<album_name.substr(0,album_name.length()-4)<<" loaded!"<<endl;
+//     cout <<"Enter character(s) to search songs from this album: "<<endl;
+//     cin>>key;
+
+//     cout<< "Showing results that contain "<<key<<" :"<<endl;
+
+//     // Demonstration of size() and max_size() in array
+//     cout<<"Size: "<<track_list.size()<<endl;
+//     cout<<"Max Size: " <<track_list.max_size()<<endl;
+//     for(i=0;i<track_list.max_size();i++){
+//         if(track_list[i].find(key)!=string::npos){
+
+//             cout<<track_list[i]<<endl;
+//         }
+        
+//     }
+//     cout<<"i at the end of the search was: "<<i<<endl;
+
+// }
+
+
+
+// Vector Implementation
+
+void songFinder(){
+
+
+    string album_name,song_name,key;
+    ifstream infs;
+    cout <<"Enter the album name (Ends with .txt): "<<endl;
+    cin>>album_name;
+    infs.open(album_name);
+    vector<string> track_list;
+    int i = 0;
+    while(getline(infs, song_name)){
+        if(!infs.fail()){
+            // track_list[i] = song_name;
+            // i++;
+
+            track_list.push_back(song_name);
+        }
+    }
+    cout<<"Album "<<album_name.substr(0,album_name.length()-4)<<" loaded!"<<endl;
+    cout <<"Enter character(s) to search songs from this album: "<<endl;
+    cin>>key;
+
+    cout<< "Showing results that contain \" "<<key<<" \" :"<<endl;
+
+    // Demonstration of size() and max_size() in array
+    cout<<"Size: "<<track_list.size()<<endl;
+    for(i=0;i<track_list.size();i++){
+        if(track_list[i].find(key)!=string::npos){
+
+            cout<<track_list[i]<<endl;
+        }
+        
+    }
+    cout<<"i at the end of the search was: "<<i<<endl;
+
+}
+
+int main(){
+    
+
+    songFinder();
+    return 0;
+}
