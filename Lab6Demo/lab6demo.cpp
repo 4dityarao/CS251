26e33fde0b4486b3a54670eabc8ebe4720c4e59d
Aditya Rao <skaditya@gmail.com>
2023-02-14 22:56:25 -0600

    Dr. Koehler Function Decomp

--- Lab6Demo/lab6demo.cpp
+++ Lab6Demo/lab6demo.cpp
@@ -4,6 +4,7 @@
 #include<sstream>
 #include<algorithm>
 #include<map>
+#include <vector>
 using namespace std;
 
 
@@ -12,48 +13,181 @@ struct movie_deets{
     int earnings;
 };
 
+void working1(const string &file_name, vector<string> &v)
+{
+    ifstream infs;
+    infs.open(file_name);
+    string line;
 
-void fix_me(const string &file_name){
-    map<string, movie_deets> movies;
-    ifstream infs(file_name);
-    string line, word, movie_name, user_input;
-    getline(infs,line); //This line reads the header so that stoi doesnt break
-    while(getline(infs,line)){
-            movie_deets temp;
-            stringstream ss(line);
-            getline(ss,movie_name,',');
-            while(getline(ss,word, ',')){
+    if(!infs.is_open())
+        return;
 
-                if(isdigit(word.at(0))){
-                    break;
-                }
-                else{
-                    if(word != " "){// This if statement basically prevents the erronous blank from being counted as a  crew member in tenet
-                        temp.crew_members.insert(word);
-                    }
-                }
-                
-            }
-            temp.earnings = stoi(word);
-        movies[movie_name] = temp;
-
+    while(getline(infs,line))
+    {
+        if (!line.empty())
+            v.push_back(line);
     }
-    do{
-        cout<<"\nEnter the movie name: ";
-        getline(cin,user_input);
-        if(movies.find(user_input)!=movies.end()){
-            cout<<"\nHere's how many crew members were in "<<user_input<<": "<< movies[user_input].crew_members.size();
-        }
-        else{
-            cout<<"\nMovie not found. Try again\n";
-        }
-    }while(user_input!="exit");
 
-        
+    infs.close();
 }
 
-int main(){
+void fun1(const auto &file_name, auto &v)
+{
+    ifstream infs;
+    infs.open(file_name);
+    string line;
 
-    fix_me("movies.txt");
+    // TODO - REMOVE - Error: No is_open() check
+
+    while(getline(infs,line))
+    {
+        // TODO - REMOVE - Error: Empty lines added
+        v.push_back(line);
+    }
+
+    infs.close();
+}
+
+
+void working2(const vector<string> &v, auto &movies)
+{
+    string word, movie_name;
+    bool first = true;
+    for (auto line : v)
+    {
+        if (first)
+        {
+            first = false;
+            continue;
+        }
+        
+        movie_deets temp;
+        stringstream ss(line);
+        getline(ss, movie_name, ',');
+
+        while(getline(ss, word, ','))
+        {
+            if(isdigit(word.at(0)))
+            {
+                break;
+            }
+            else if (!word.empty())
+            {
+                temp.crew_members.insert(word);
+            }
+        }
+
+        temp.earnings = stoi(word);
+        movies[movie_name] = temp;
+    }
+}
+
+void fun2(const auto &v, auto &movies)
+{
+    string word, movie_name;
+    bool first = true;
+    for (auto line : v)
+    {
+        // TODO - REMOVE - Error: First line is not skipped
+        
+        movie_deets temp;
+        stringstream ss(line);
+        getline(ss, movie_name, ',');
+
+        while(getline(ss, word, ','))
+        {
+            if(isdigit(word.at(0)))
+            {
+                break;
+            }
+            // TODO - REMOVE - Error: Empty lines are inserted
+            temp.crew_members.insert(word);
+        }
+
+        temp.earnings = stoi(word);
+        movies[movie_name] = temp;
+    }
+}
+
+string trim(const string &input)
+{
+    int start, end;
+    string s = input;
+
+    start = -1;
+    while(start < s.size() && isspace(s.at(++start)));   
+
+    if (start > 0)
+        s = s.substr(start);
+
+    end = s.size();
+    while(end > 0 && isspace(s.at(--end)));
+
+    if (end+1 < s.size())
+        s = s.substr(0, end+1);
+
+    return s;
+}
+
+void working3(const auto &movies)
+{
+    string user_input;
+    
+    while(trim(user_input) != "exit")
+    {
+        cout << "Enter the movie name: " << endl;
+        getline(cin, user_input);
+        if (movies.count(user_input) != 0)
+        {
+            cout << endl << "Here's how many crew members were in "
+                << user_input << ": " 
+                << movies.at(user_input).crew_members.size()
+                << endl << endl;
+        }
+        else if (movies.count(user_input) == 0 && trim(user_input) != "exit")
+        {
+            cout << endl << "Movie not found. Try again" << endl << endl;
+        }
+    }     
+}
+
+// TODO - REMOVE - Error: cannot use [] with const
+// void broken3(const map<string, movie_deets> &movies)
+void fun3(auto &movies)
+{
+    string user_input;
+    do
+    {
+        cout << "Enter the movie name: " << endl;
+        getline(cin, user_input);
+        if(user_input != "exit") // TODO - REMOVE - whitespace in getline prevents match 
+        {
+            // TODO - REMOVE - Error - Adds to map
+            cout << endl << "Here's how many crew members were in "
+                    << user_input << ": " 
+                    << movies[user_input].crew_members.size()
+                    << endl << endl;
+        }
+    }while(user_input != "exit");   // TODO - REMOVE - whitespace in getline prevents match   
+}
+
+
+int main(){
+    vector<string> lines;
+    map<string, movie_deets> m;
+
+    // working1("movies.txt", lines);
+    // working2(lines, m);
+    // working3(m);
+
+    // reset states
+    lines.clear();
+    m.clear();
+
+    // Call the broken versions
+    fun1("movies.txt", lines);
+    fun2(lines,m);
+    fun3(m);
+
     return 0;
 }
